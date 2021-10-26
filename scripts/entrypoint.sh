a49df976b6d2409580d5c537cc1f06871081f35c
Fauzia Shaikh <fauzia.shaikh@rallyhealth.com>
2021-10-25 20:16:18 -0500

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 airflow db init
 airflow users create -r Admin -u admin -e admin@example.com -f admin -l user -p admin1234
+airflow connections add 'postgres_fraud_etl' \
+    --conn-type 'postgres' \
+    --conn-login 'airflow' \
+    --conn-password 'airflow' \
+    --conn-host 'postgres' \
+    --conn-port '5432' \
+    --conn-schema 'airflow'
 airflow webserver
